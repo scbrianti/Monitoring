24347f77a6bc6d936cf046cd89fd27dc539f7c7c
Sidnei Brianti <scbrianti@gmail.com>
2018-01-09 12:34:10 -0200

    Update config.php

--- config.php
+++ config.php
@@ -9,6 +9,6 @@ require_once('./phpagi/phpagi_asmanager.php');
 $manager_host = '127.0.0.1';
 
 // USUARIO CRIADO NO MANAGER.CONF
-$manager_user = 'dongles' ;		
-$manager_pass = '95ef5abdc4c2880eeb35032c4a7a1133';
+$manager_user = 'snep' ;		
+$manager_pass = 'sneppass';
 ?>
